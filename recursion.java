f1c38506ff45a1bfd544b71e4ed5807124d9bc43
LORN-Hongnida <137864918+LORN-Hongnida@users.noreply.github.com>
2023-11-23 09:09:38 +0700

    Update recursion.java

--- recursion.java
+++ recursion.java
@@ -1,15 +1,31 @@
 public class recursion {
-  public static void main(String[] args) {
-
-    //call your recursion fuction
-    //print result
-
-   }
-    
-    public double.... recursionFunction {
-      //implement your case algorithm
-      return....
+  // Recursive Approach
+    public static int powerRecursive(int x, int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return x * powerRecursive(x, n - 1);
+        }
     }
-  
-}
-    
+    // Normal Loop
+    public static int powerIterative(int x, int n) {
+        int result = 1;
+        for (int i = 0; i < n; i++) {
+            result *= x;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int x = 5;
+        int n = 4;
+
+        int powerRecursiveResult = powerRecursive(x, n);
+
+        int powerIterativeResult = powerIterative(x, n);
+
+        System.out.println("Recursive power of " + x + " raised to " + n + " is " + powerRecursiveResult);
+
+        System.out.println("Iterative power of " + x + " raised to " + n + " is " + powerIterativeResult);
+    }
+}   
